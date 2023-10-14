08903ccc0becf7c08bf1ecc7759e2495fb41e0b2
D4nf313 <danfecisf@gmail.com>
2023-10-14 10:05:12 -0500

    Solucion error conexion bd

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+@Module({
+  imports: [
+
+    AuthModule,
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AuthModule {
+
+
+
+}
